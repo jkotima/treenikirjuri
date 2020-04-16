38942815f96c379c84e21c6ecba4cdbcee247cb7
Juuso Kotimäki <jkotima@helsinki.fi>
2020-04-16 23:49:21 +0300

    validator bug fix

--- application/exercises/forms.py
+++ application/exercises/forms.py
@@ -4,15 +4,15 @@ from wtforms import RadioField, StringField, BooleanField, validators
 from application.exercises.models import Exercises
 
 class ExerciseForm(FlaskForm):
-    name = StringField("Nimi", [validators.Length(min=2, max=144)])
-    description = StringField("Kuvaus", [validators.Length(min=2, max=144)])
+    name = StringField("Nimi", [validators.Length(max=144)])
+    description = StringField("Kuvaus", [validators.Length(max=144)])
     unit = RadioField('Toistoissa käytetty yksikkö', choices=[('kg','Kilogramma'),('min','Minuutti'),('sec', 'Sekunti')], default='kg')
     
     class Meta:
         csrf = False
 
 class ExerciseEditForm(FlaskForm):
-    name = StringField("Korvaava nimi", [validators.Length(min=2, max=144)])
+    name = StringField("Korvaava nimi", [validators.Length(max=144)])
     description = StringField("Korvaava kuvaus", [validators.Length(max=144)])
     unit = RadioField('Korvaava toistoissa käytetty yksikkö', choices=[('kg','Kilogramma'),('min','Minuutti'),('sec', 'Sekunti')])
 
