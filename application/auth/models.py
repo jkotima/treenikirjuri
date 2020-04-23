bd2d40860900fbfd8374b97d393733126b06e296
Juuso Kotimäki <jkotima@helsinki.fi>
2020-04-23 19:12:10 +0300

    deleted table user_program, added active_program -field to accounts

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,12 +1,6 @@
 from application import db
 from application.models import Base
 
-#liitostaulu user_program
-user_program = db.Table('user_program',
-    db.Column('user_id', db.Integer, db.ForeignKey('accounts.id'), primary_key=True),
-    db.Column('program_id', db.Integer, db.ForeignKey('programs.id'), primary_key=True)
-)
-
 class Users(Base):
 
     __tablename__ = "accounts"
@@ -14,14 +8,11 @@ class Users(Base):
     name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
     password = db.Column(db.String(144), nullable=False)
+    active_program = db.Column(db.Integer, db.ForeignKey('programs.id'),
+                        nullable=True)
 
-    exercises = db.relationship('Exercises', backref='Accounts', lazy=True)
-    events = db.relationship('Events', backref='Accounts', lazy=True)
-    programs = db.relationship('Programs', backref='Accounts', lazy=True)
-
-    #user_program
-    programs = db.relationship('Programs', secondary=user_program, lazy='subquery',
-        backref=db.backref('Users', lazy=True))    
+    exercises = db.relationship('Exercises', backref='Users', lazy=True)
+    events = db.relationship('Events', backref='Users', lazy=True)
 
     def __init__(self, name, username, password):
         self.name = name
